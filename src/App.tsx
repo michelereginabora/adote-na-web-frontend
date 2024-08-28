3ca7f294e534b393ff12f0c40c38abc1bca7f6ed
Michele Regina Bora <106915522+michelereginabora@users.noreply.github.com>
2024-08-28 11:02:54 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ const App: React.FC = () => {
       <div>
         <Routes>
           <Route path="/adote-na-web-frontend" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
           <Route path="/adote-na-web-frontend/emconstrucao" element={<PageUnderConstruction backgroundImageSrc={'assets/cat.png'} />} />
           {/* <Route path="/adotar" element={<AdoptionPage getAnimalsUseCase={getAnimalsUseCase} />} /> */}
         </Routes>
