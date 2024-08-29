8bf8d6a576c67fd88baba93c8bda6c8f8727ddc5
Michele Regina Bora <miregina@live.com>
2024-08-29 16:49:37 -0300

    updates styles

--- src/presentation/components/LandingPage/Section.tsx
+++ src/presentation/components/LandingPage/Section.tsx
@@ -42,7 +42,7 @@ const Section: React.FC = () => {
             <ul role="list" className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-[var(--color-text-light)] sm:grid-cols-2 sm:gap-6">
               {features.map((feature, index) => (
                 <li key={index} className="flex gap-x-3">
-                  <svg className="h-6 w-5 flex-none text-[var(--color-text-dark)]" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <svg className="h-6 w-5 flex-none text-[var(--color-text-section)]" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path
                       fillRule="evenodd"
                       d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
