2867a8717d8792218328506918443b0442389a78
Michele Regina Bora <miregina@live.com>
2024-09-04 13:48:25 -0300

    chore: update path

--- src/presentation/components/Button/LoginButton.tsx
+++ src/presentation/components/Button/LoginButton.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const LoginButton: React.FC = () => (
   <div className="hidden lg:flex lg:flex-1 lg:justify-end">
     <Link
-      to="/emconstrucao"
+      to="emconstrucao"
       className="bg-[var(--color-primary-light)] text-[var(--color-primary-dark)] text-sm font-semibold leading-6 py-3 px-6 rounded-md shadow-md hover:bg-[var(--color-primary-dark)] hover:text-[var(--color-primary-light)] transition duration-300 ease-in-out"
     >
       Entrar <span aria-hidden="true">&rarr;</span>
